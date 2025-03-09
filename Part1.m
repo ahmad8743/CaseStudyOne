e43bf25f6c3ad858dde0c0939fe9888ce1559b69
Ahmad Hamzeh <hamzehahmad06@hotmail.com>
2025-03-09 14:14:24 -0500

    new system functions

--- Part1.m
+++ Part1.m
@@ -9,15 +9,8 @@
 % immigration regularly exhibits the same asymptotic stability found in
 % coexisting populations in the wild.
 
-function dsdt = system(t, sys)
-    % hyper parameters
-    r = 0.1;
-    a = 0.1;
-    b = 0.3;
-    m = 0.2;
-    c = 0.01;
-    d = 0;
-
+% this function makes is easy to pass in hyperparamters
+function dsdt = system(t, sys, r, a, b, m, c, d)
     % system
     dsdt = [r * sys(1) - a * sys(1) * sys(2) + c 
             % dx = rx - axy + c
@@ -25,13 +18,26 @@ function dsdt = system(t, sys)
             % dy = bxy - my + d
 end
 
+% similar to above but handles cases where C(x) = c/x and D(x) = d/x
+function dsdt = system_x(t, sys, r, a, b, m, c, d)
+    % system
+    dsdt = [r * sys(1) - a * sys(1) * sys(2) + c / sys(1); 
+            % dx = rx - axy + c
+            b * sys(1) * sys(2) - m * sys(2) + d / sys(2)];
+            % dy = bxy - my + d
+end
+
 tspan = [0 1500];
 initial = [5 5];
-[t, s1] = ode45(@system, tspan, initial);
+test1 = @(t, x) system(t, x, 0.1, 0.1, 0.3, 0.2, 0.01, 0);
+[t1, s1] = ode45(test1, tspan, initial);
+test2 = @(t, x) system_x(t, x, 0.1, 0.1, 0.3, 0.2, 0.01, 0);
+[t2, s2] = ode45(test2, tspan, initial);
+
 
 figure(1);
 subplot(1, 2, 1)
-plot(t, s1, 'LineWidth', 2);
+plot(t1, s1, 'LineWidth', 2);
 xlim([-100 1500]); 
 xticks(0:200:1500);
 ylim([-1 15]);
@@ -39,6 +45,21 @@ yticks(0:2:14);
 
 subplot(1, 2, 2);
 plot(s1(:, 1), s1(:, 2));
+exportgraphics(gca, 'fig1.png');
+
+figure(2);
+subplot(1, 2, 1)
+plot(t2, s2, 'LineWidth', 2);
+xlim([-100 1500]); 
+xticks(0:200:1500);
+ylim([-1 15]);
+yticks(0:2:14);
+
+subplot(1, 2, 2);
+plot(s2(:, 1), s2(:, 2));
+
+% rest of plots are easy...
+
 
 % Constant migration: predictable patterns that can most likely be backed
 % by studied behavior of a given species. Possible enterpretations of
